5a4a2a406204f8f6aee2e985d2e382d348675dac
seungahc <seungahc@tripbtoz.com>
2025-05-26 17:18:07 +0900

    Package.swift 수정

--- Package.swift
+++ Package.swift
@@ -1,10 +1,14 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "ISPageControl",
+    platforms: [
+        .iOS(.v11),
+        .macOS(.v10_15)
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -20,9 +24,14 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "ISPageControl",
-            dependencies: []),
+            dependencies: [],
+            path: "Sources",
+            resources: [
+                .process("Resource")
+            ]),
         .testTarget(
             name: "ISPageControlTests",
-            dependencies: ["ISPageControl"]),
+            dependencies: ["ISPageControl"],
+            path: "Tests"),
     ]
 )
